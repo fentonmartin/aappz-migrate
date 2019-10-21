d8dc697c26ab6bf1f926f0f55b413a19bcceda3c
Fenton Martin <fentonxm@gmail.com>
2019-10-21 12:34:18 +0700

    Update main activity

--- app/src/main/java/io/github/fentonmartin/aappz/migrate/MainActivity.java
+++ app/src/main/java/io/github/fentonmartin/aappz/migrate/MainActivity.java
@@ -1,14 +1,35 @@
 package io.github.fentonmartin.aappz.migrate;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import io.github.fentonmartin.aappz.AappZ;
+
+public class MainActivity extends AappZ {
+
+    private Button buttonAction;
+    private TextView textContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonAction = findViewById(R.id.migrate_action);
+        textContent = findViewById(R.id.migrate_content);
+
+        setLayout("This is an example of content migration", "Migrate Now!");
+    }
+
+    private void setLayout(String text, String button) {
+        setLayout(text, button, null);
+    }
+
+    private void setLayout(String text, String button, View.OnClickListener listener) {
+        buttonAction.setOnClickListener(listener);
+        buttonAction.setText(button);
+        textContent.setText(text);
     }
 }
