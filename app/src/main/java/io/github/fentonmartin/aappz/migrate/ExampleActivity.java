c1e868227af8c69967a8a0957fc4a3acb0e7fe36
Fenton Martin <fentonxm@gmail.com>
2019-10-21 13:57:56 +0700

    Add setActivity MainActivity

--- app/src/main/java/io/github/fentonmartin/aappz/migrate/ExampleActivity.java
+++ app/src/main/java/io/github/fentonmartin/aappz/migrate/ExampleActivity.java
@@ -10,5 +10,7 @@ public class ExampleActivity extends AappZ {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_example);
+
+        setActivity(MainActivity.class);
     }
 }
